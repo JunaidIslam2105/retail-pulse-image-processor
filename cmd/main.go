7065646e20f1d8640dda0ed27b8e0347d8e2e8c2
Junaid Islam <Junaid.2005p@gmail.com>
2025-01-23 16:37:57 +0530

    Updated README

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,23 @@
-package cmd
+package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"retail-pulse-image-processor/internal/handlers"
+	"retail-pulse-image-processor/internal/services"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println(w, "Retail pulse Image Processor is working")
-	})
+	err := services.LoadStoreMaster("../config/StoreMasterAssignment.csv")
+	if err != nil {
+		log.Fatalf("Error loading StoreMaster: %v\n", err)
+	}
 
-	fmt.Println("Server is running on port 8080")
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/api/submit", handlers.SubmitJobHandler)
+	http.HandleFunc("/api/status", handlers.JobStatusHandler)
+
+	port := ":8080"
+	fmt.Printf("Server is running on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
